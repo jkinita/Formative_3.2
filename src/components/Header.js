2eb0270083c5fe1039b3fdc8ded367a0c0f91fa9
jkinita <jkinita@yahoo.com>
2020-03-11 11:50:18 +1300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Header extends React.Component {
+  render() {
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Header;
