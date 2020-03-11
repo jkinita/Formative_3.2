e031a1f15c730a4bbf800d04cf88815ef4e0971f
demigraty <demi.graty@gmail.com>
2020-03-12 12:05:13 +1300

    working on branch 2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     return (
       <div className="container-lg">
         <h1>Hello Worldy</h1>
+        <h1>working on branch 2</h1>
         <Header />
       </div>
     );
