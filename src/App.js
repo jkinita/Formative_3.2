a6823454f891b977d8ab8f611cd72edddebb8676
demigraty <demi.graty@gmail.com>
2020-03-12 09:44:00 +1300

    h1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 class App extends React.Component {
   render() {
-    return(
+    return (
       <div className="container-lg">
+        <h1>Hello World</h1>
         <Header />
       </div>
     );
