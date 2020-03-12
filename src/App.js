23b13c09e26398b1c9140f6a854bf586322aaaa7
jkinita <jkinita@yahoo.com>
2020-03-13 09:11:21 +1300

    getdata and data files made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Data from './components/Data'
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-lg">
-        <h1>Hello Worldy</h1>
         <Header />
+        <Data />
       </div>
     );
   }
