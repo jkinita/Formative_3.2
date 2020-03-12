7fbcfef43536228e625dab9626fde83110d9ae94
jkinita <jkinita@yahoo.com>
2020-03-13 09:25:39 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="container-lg">
+      <h1>Hello Worldy</h1>
+        <h1>working on branch 2</h1>
+        <h1>Busy working on branch 2, at home</h1>
         <Header />
         <Data />
       </div>
